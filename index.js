ffefa8d864713ef7836ce7b77349d16149e4aca5
Kenneth Hau <47057278+kitchun0402@users.noreply.github.com>
2020-04-12 16:46:30 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+// Detect Mouse Click
+var allButtons = document.querySelectorAll(".drum");
+for (var i = 0; i < allButtons.length; i++) {
+  allButtons[i].addEventListener("click", function(){
+    var isSuccessful = makeSound(this.innerHTML);
+    if (isSuccessful){
+      buttonAnimation(this.innerHTML);
+    }
+  });
+}
+
+// Detect Keyboard Press
+document.addEventListener("keydown", function(event) {
+  var isSuccessful = makeSound(event.key);
+  if (isSuccessful){
+    buttonAnimation(event.key);
+  }
+
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      return true;
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      return true;
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      return true;
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      return true;
+      break;
+    case "j":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      return true;
+      break;
+    case "k":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      return true;
+      break;
+    case "l":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      return true;
+      break;
+    default:
+      return false;
+  }
+}
+
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("." + currentKey)
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
